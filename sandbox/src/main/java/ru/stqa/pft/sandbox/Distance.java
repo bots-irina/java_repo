c4b55db56f3cfc83c0ab6f1e813a9b3f1312bd37
bots-irina <irina-demen@mail.ru>
2026-08-27 13:15:05 +0400

    Добавлен конструктор в Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -4,13 +4,8 @@ public class Distance {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point();
-    p1.x = 1;
-    p1.y = 2;
-
-    Point p2 = new Point();
-    p2.x = 4;
-    p2.y = 6;
+    Point p1 = new Point(1, 2);
+    Point p2 = new Point(4, 6);
 
     System.out.println("Расстояние между точками c координатами " + "p1(" + p1.x + ";" + p1.y + ") " + "и " + "p2(" + p2.x + ";" + p2.y + ")" + " = " + distance(p1, p2));
   }
